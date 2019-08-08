f79cf0167f4bc047e0486413ed3d9578ee94033b
Sam M <sammontalvojr@gmail.com>
2019-08-08 17:31:52 -0500

    front end setup and initial stripe setup

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,12 +11,18 @@ module.exports = app => {
   );
 
   //successful google login
-  app.get("/auth/google/callback", passport.authenticate("google"));
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/surveys");
+    }
+  );
 
-  app.get("/api/logout", (req,res)=>{
+  app.get("/api/logout", (req, res) => {
     req.logout();
-    res.send(req.user);
-  })
+    res.redirect('/');
+  });
 
   app.get("/api/current_user/", (req, res) => {
     res.send(req.user);
